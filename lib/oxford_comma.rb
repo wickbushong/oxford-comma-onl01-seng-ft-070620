96fb4129332e4aac9f5fc7092089ddb4e5fdc5b4
Wick Bushong <wickbushong@gmail.com>
2020-07-07 15:45:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  ans = ""
+  array.each_with_index {|element, i|
+    case i
+      when 0
+        ans << element
+      when array.length-1
+        if array.length == 2
+          ans << " and #{element}"
+        else
+          ans << ", and #{element}"
+        end
+      else
+        ans << ", #{element}"
+    end
+  }
+  ans
 end
